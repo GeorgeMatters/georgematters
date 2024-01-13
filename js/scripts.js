c923c52f73e6aebe934af0ad35ac0a7d95bc4d8f
MarcusReitzl <mareitzl@edu.aau.at>
2024-01-13 03:07:08 +0100

    release version 0.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,135 +1,3 @@
-// Function to load a HTML file into a target div and execute a callback after loading
-function loadHTML(url, id, callback) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            const element = document.getElementById(id);
-            if (element) {
-                element.innerHTML = data;
-                if (callback) callback();
-            }
-        }).catch(error => console.error('Error loading HTML:', error));
-}
-
-// AHome Slider
-
-function initHomeSlider() {
-    const slides = document.querySelectorAll('.home-section .home-slide');
-    let currentSlide = 0;
-
-    function showNextSlide() {
-        // Remove 'active' class from current slide
-        slides[currentSlide].classList.remove('active');
-
-        // Compute the next slide index
-        currentSlide = (currentSlide + 1) % slides.length;
-
-        // Add 'active' class to the new current slide
-        slides[currentSlide].classList.add('active');
-    }
-
-    // Start the slideshow
-    setInterval(showNextSlide, 5000); // Change slide every 5000ms (5 seconds)
-}
-
-
-// Attach event listeners to navigation links
-function attachNavListeners() {
-    const header = document.querySelector('.site-header');
-    const headerHeight = header ? header.offsetHeight : 0;
-
-    // Select all anchor tags with href starting with '#'
-    document.querySelectorAll('a[href^="#"]').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            if (targetElement) {
-                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-                window.scrollTo({
-                    top: targetPosition,
-                    behavior: 'smooth'
-                });
-            }
-        });
-    });
-}
-
-// Initialize the contact form
-function initializeContactForm() {
-    const contactForm = document.getElementById('contactForm');
-    if (!contactForm) return;
-
-    contactForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        let isFormValid = true;
-
-        Array.from(contactForm.elements).forEach(input => {
-            if (input.type !== "submit" && !validateInput(input)) {
-                isFormValid = false;
-            }
-        });
-
-        if (isFormValid) {
-            alert('Thank you for your message! We will get back to you soon.');
-        } else {
-            console.log('Please fill out all the fields correctly.');
-        }
-    });
-
-    function validateInput(input) {
-        const isValid = input.checkValidity();
-        input.parentElement.classList.toggle('invalid', !isValid);
-        return isValid;
-    }
-}
-
-// Slider functionality
-function startSlideshow() {
-    const slider = document.getElementById('image-slider');
-    const slides = Array.from(slider.getElementsByClassName('slide'));
-    let currentIndex = 0;
-
-    function updateSlider() {
-        slides.forEach((slide, index) => {
-            slide.classList.remove('active');
-            if (index === currentIndex || index === (currentIndex + 1) % slides.length || index === (currentIndex + 2) % slides.length) {
-                slide.classList.add('active');
-            }
-        });
-    }
-
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
-        updateSlider();
-    }
-
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-        updateSlider();
-    }
-
-    slider.querySelector('.next').addEventListener('click', nextSlide);
-    slider.querySelector('.prev').addEventListener('click', prevSlide);
-
-    // Automatic movement
-    setInterval(nextSlide, 5000);
-
-    updateSlider(); // Initialize the slider
-}
-
-// Load and initialize sections
-document.addEventListener("DOMContentLoaded", function() {
-    loadHTML('html/header.html', 'header', attachNavListeners);
-    loadHTML('html/home.html', 'home', initHomeSlider);
-    loadHTML('html/about.html', 'about');
-    loadHTML('html/slider.html', 'slider', startSlideshow);
-    loadHTML('html/socialmedia.html', 'social');
-    loadHTML('html/services.html', 'services');
-    loadHTML('html/projects.html', 'projects');
-    loadHTML('html/contact.html', 'contact', initializeContactForm);
-    loadHTML('html/footer.html', 'footer', attachNavListeners);
-});
 
 
 // Function to load a HTML file into a target div and execute a callback after loading
@@ -210,39 +78,49 @@ function initializeContactForm() {
         }
     }
 
-    // Functionality for the image slider
-    function startSlideshow() {
+function startSlideshow() {
     const slider = document.getElementById('image-slider');
     const slides = Array.from(slider.getElementsByClassName('slide'));
     let currentIndex = 0;
+
     function updateSlider() {
+        if (currentIndex >= slides.length) {
+            currentIndex = 0; // Reset to first slide
+        }
+        const firstIndex = currentIndex;
+        const secondIndex = (firstIndex + 1) % slides.length;
+        const thirdIndex = (secondIndex + 1) % slides.length;
+
         slides.forEach((slide, index) => {
-            slide.classList.remove('active');
-            if (index === currentIndex || index === (currentIndex + 1) % slides.length || index === (currentIndex + 2) % slides.length) {
-                slide.classList.add('active');
+            slide.style.display = 'none';
+            if (index === firstIndex || index === secondIndex || index === thirdIndex) {
+                slide.style.display = 'flex';
             }
         });
     }
-    
+
     function nextSlide() {
-        currentIndex = (currentIndex + 1) % slides.length;
+        currentIndex++;
         updateSlider();
     }
-    
+
     function prevSlide() {
-        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        currentIndex--;
+        if (currentIndex < 0) {
+            currentIndex = slides.length - 1; // Loop to last slide
+        }
         updateSlider();
     }
-    
+
     slider.querySelector('.next').addEventListener('click', nextSlide);
     slider.querySelector('.prev').addEventListener('click', prevSlide);
     
     // Automatic movement
-    setInterval(nextSlide, 5000); // Change slide every 5 seconds
-    
+    setInterval(nextSlide, 4000); // Change slide every 4 seconds
+
     updateSlider(); // Initialize the slider
 }
-
+    
 // Load and initialize sections when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
 loadHTML('html/header.html', 'header', attachNavListeners);
@@ -250,8 +128,8 @@ loadHTML('html/home.html', 'home', initHomeSlider);
 loadHTML('html/about.html', 'about');
 loadHTML('html/slider.html', 'slider', startSlideshow);
 loadHTML('html/socialmedia.html', 'social');
-loadHTML('html/services.html', 'services');
+loadHTML('html/services.html', 'services', attachNavListeners);
 loadHTML('html/projects.html', 'projects');
 loadHTML('html/contact.html', 'contact', initializeContactForm);
-loadHTML('html/footer.html', 'footer');
+loadHTML('html/footer.html', 'footer', attachNavListeners);
 });    
